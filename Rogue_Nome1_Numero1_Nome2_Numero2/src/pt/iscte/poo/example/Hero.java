d86ed7480f5c0bef23d3ba24ef8448981797ea96
VicenteSilva03 <117606704+VicenteSilva03@users.noreply.github.com>
2022-11-24 10:37:57 +0000

    Inventory and Movable rework
    Manual update of the class Hero.

--- Rogue_Nome1_Numero1_Nome2_Numero2/src/pt/iscte/poo/example/Hero.java
+++ Rogue_Nome1_Numero1_Nome2_Numero2/src/pt/iscte/poo/example/Hero.java
@@ -1,6 +1,6 @@
 package pt.iscte.poo.example;
 
-import java.util.ArrayList;
+import java.awt.event.KeyEvent;
 
 import pt.iscte.poo.gui.ImageMatrixGUI;
 import pt.iscte.poo.utils.Direction;
@@ -11,10 +11,10 @@ public class Hero extends Movable {
 
 	private static final int MAXIMUM_HP = 10;
 	private static final int DAMAGE = -1;
+	private static final int DEFAULT_HERO_LAYER = 1;
 
 	private double dodgeChance = 0;
-
-	private ArrayList<GameElement> inventory = new ArrayList<>();
+	private GameElement[] inventory = new GameElement[3];
 
 	public Hero(Point2D point) {
 		super(point, "Hero", MAXIMUM_HP, DAMAGE);
@@ -26,11 +26,16 @@ public class Hero extends Movable {
 	}
 
 	@Override
+	public int getLayer() {
+		return DEFAULT_HERO_LAYER;
+	}
+
+	@Override
 	public void move(Direction dir) {
 		Vector2D vector = dir.asVector();
 		Point2D newPoint = super.position.plus(vector);
 		GameElement e = GameEngine.getInstance().getCurrentRoom().getElement(newPoint);
-		if (e instanceof Pickable) {
+		if (e instanceof Item) {
 			pick(e);
 		}
 		super.move(dir);
@@ -38,15 +43,45 @@ public class Hero extends Movable {
 			Door d = (Door) e;
 			if (d.isOpen())
 				GameEngine.getInstance().manageRoom(d.getRoomName(), d.getHeroPoint());
-			else
-				for (GameElement i : inventory) {
-					if (i instanceof Key) {
-						Key k = (Key) i;
-						if (k.getCode().equals(d.getKeyCode()))
-							d.open();
-					}
-				}
+			else if (hasKey(d.getKeyCode()))
+				d.open();
 		}
+	}
+
+	public void pick(GameElement e) {
+		if (!addItem(e))
+			return;
+		GameEngine.getInstance().getCurrentRoom().removeElement(e);
+		ImageMatrixGUI.getInstance().removeImage(e);
+		((Item) e).pick();
+	}
+
+	public boolean addItem(GameElement e) {
+		int index = getSlot();
+		if (index < 0) {
+			System.out.println("Inventory is Full!");
+			return false;
+		} else {
+			e.setPosition(new Point2D(MAXIMUM_HP / 2 + index, GameEngine.GRID_HEIGHT));
+			inventory[index] = e;
+			GameEngine.getInstance().gui.addImage(e);
+			return true;
+		}
+	}
+
+	public int getSlot() {
+		for (int i = 0; i < inventory.length; i++)
+			if (inventory[i] == null)
+				return i;
+		return -1;
+	}
+
+	public boolean hasKey(String doorKey) {
+		for (GameElement e : inventory)
+			if (e instanceof Key)
+				if (((Key) e).getCode().equals(doorKey))
+					return true;
+		return false;
 	}
 
 	@Override
@@ -54,23 +89,11 @@ public class Hero extends Movable {
 		m.setHitpoints(super.damage);
 	}
 
-	public void pick(GameElement e) {
-		inventory.add(e);
-		GameEngine.getInstance().getCurrentRoom().removeElement(e);
-		ImageMatrixGUI.getInstance().removeImage(e);
-		((Pickable) e).pick();
-	}
-
 	@Override
 	public void setHitpoints(int value) {
 		if (Math.random() > dodgeChance)
-			super.hitpoints += value;
+			super.setHitpoints(value);
 		updateHealthBar(super.hitpoints);
-		System.out.println(getName() + super.hitpoints);
-	}
-
-	public void setPosition(Point2D point) {
-		super.position = point;
 	}
 
 	public void scaleDamage(int value) {
@@ -86,7 +109,43 @@ public class Hero extends Movable {
 				code = "GreenRed";
 			if (i * 2 >= hitpoints)
 				code = "Red";
-			GameEngine.getInstance().gui.addImage(GameElement.create(code, new Point2D(i, GameEngine.GRID_HEIGHT), null));
+			GameEngine.getInstance().gui
+					.addImage(GameElement.create(code, new Point2D(i, GameEngine.GRID_HEIGHT), null));
 		}
 	}
+
+	public void updateInventory() {
+		for (GameElement e : inventory) {
+			if (e == null)
+				break;
+			GameEngine.getInstance().gui.addImage(e);
+		}
+	}
+
+	public void updateHeroBar() {
+		updateHealthBar(hitpoints);
+		updateInventory();
+	}
+
+	public void drop(int key) {
+		int index = keyEventToInt(key);
+		GameElement e = inventory[index];
+		if (e == null)
+			return;
+		inventory[index] = null;
+		e.setPosition(super.position);
+		GameEngine.getInstance().getCurrentRoom().addElement(e);
+	}
+
+	public int keyEventToInt(int key) {
+		switch (key) {
+		case KeyEvent.VK_1:
+			return 0;
+		case KeyEvent.VK_2:
+			return 1;
+		default:
+			return 2;
+		}
+	}
+
 }
