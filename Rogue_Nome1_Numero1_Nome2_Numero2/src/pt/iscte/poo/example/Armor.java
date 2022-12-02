46c831807ef9fba229d47b7b73de636eadf831aa
VicenteSilva03 <117606704+VicenteSilva03@users.noreply.github.com>
2022-12-02 19:26:34 +0000

    Hero bar finished. Several Errors fixed.

--- Rogue_Nome1_Numero1_Nome2_Numero2/src/pt/iscte/poo/example/Armor.java
+++ Rogue_Nome1_Numero1_Nome2_Numero2/src/pt/iscte/poo/example/Armor.java
@@ -9,17 +9,4 @@ public class Armor extends Item {
 	public Armor(Point2D point) {
 		super(point, "Armor");
 	}
-
-	@Override
-	public void pick(GameElement e) {
-		GameEngine.getInstance().getHero().setDodgeChance(DODGE_CHANCE);
-		super.pick(e);
-	}
-
-	@Override
-	public void drop(int index) {
-		GameEngine.getInstance().getHero().setDodgeChance(0.0);
-		super.drop(index);
-	}
-
 }
