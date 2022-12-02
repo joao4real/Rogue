8847ddb01f713f2e4a2540831e36db19f662787d
VicenteSilva03 <117606704+VicenteSilva03@users.noreply.github.com>
2022-12-02 19:38:56 +0000

    Hero bar finished. Several Errors fixed.

--- Rogue_Nome1_Numero1_Nome2_Numero2/src/pt/iscte/poo/example/Item.java
+++ Rogue_Nome1_Numero1_Nome2_Numero2/src/pt/iscte/poo/example/Item.java
@@ -17,13 +17,13 @@ public abstract class Item extends GameElement {
 	}
 
 	public boolean addItem(GameElement e) {
-		Hero hero = GameEngine.getInstance().getHero();
+		Hero hero = Hero.getInstance();
 		int index = hero.getSlot();
 		if (index < 0) {
 			System.out.println("Inventory is Full!");
 			return false;
 		} else {
-			e.setPosition(new Point2D(Hero.MAXIMUM_HP / 2 + index, GameEngine.GRID_HEIGHT));
+			e.setPosition(new Point2D(Hero.MAXIMUM_HP / 2 + index + 2, GameEngine.GRID_HEIGHT));
 			hero.getInventory()[index] = e;
 			GameEngine.getInstance().gui.addImage(e);
 			return true;
@@ -31,10 +31,10 @@ public abstract class Item extends GameElement {
 	}
 
 	public void drop(int index) {
-		GameElement[] inventory = GameEngine.getInstance().getHero().getInventory();
+		GameElement[] inventory = Hero.getInstance().getInventory();
 		GameElement e = inventory[index];
 		inventory[index] = null;
-		e.setPosition(GameEngine.getInstance().getHero().getPosition());
+		e.setPosition(Hero.getInstance().getPosition());
 		GameEngine.getInstance().getCurrentRoom().addElement(e);
 	}
 }
