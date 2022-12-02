e826ce63567d3061e98c74c1473bea140f698929
VicenteSilva03 <117606704+VicenteSilva03@users.noreply.github.com>
2022-12-02 19:39:36 +0000

    Hero bar finished. Several Errors fixed.

--- Rogue_Nome1_Numero1_Nome2_Numero2/src/pt/iscte/poo/example/Scorpion.java
+++ Rogue_Nome1_Numero1_Nome2_Numero2/src/pt/iscte/poo/example/Scorpion.java
@@ -3,23 +3,23 @@ package pt.iscte.poo.example;
 import pt.iscte.poo.utils.Direction;
 import pt.iscte.poo.utils.Point2D;
 
-public class Scorpio extends Movable {
+public class Scorpion extends Movable {
 
 	public static final int MAXIMUM_HP = 2;
 	public static final int DAMAGE = -1;
 
-	public Scorpio(Point2D point) {
+	public Scorpion(Point2D point) {
 		super(point, "Scorpio", MAXIMUM_HP, DAMAGE);
 	}
 
 	@Override
 	public void move(Direction d) {
-		super.move(Direction.forVector(super.position.vectorTo(GameEngine.getInstance().getHero().getPosition())));
+		super.move(Direction.forVector(super.position.vectorTo(Hero.getInstance().getPosition())));
 	}
 
 	@Override
 	public void attack(Movable m) {
 		if (m instanceof Hero)
-			GameEngine.getInstance().getHero().setPoisonStatus(true);
+			Hero.getInstance().setPoisonStatus(true);
 	}
 }
